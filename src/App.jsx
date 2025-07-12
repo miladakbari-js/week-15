69fed202b5e5b1c262d2d6294db5ae9f08e21d27
Milad Akbari <milad.akbari.codevelop@gmail.com>
2025-07-12 15:48:15 +0330

    REGEX Validation & fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,30 @@ import cities from "./cities.json";
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [hint, setHint] = useState("");
+  const [error , setError] = useState(false)
 
   const handleChange = (event) => {
     const value = event.target.value;
     setInputValue(value);
     const closestMatch = cities.find((city) => city.startsWith(value)) || "";
     setHint(value ? closestMatch : "");
+
+    const invalidPattern =/^[A-Z][a-z]*$/;
+
+    setError(value !== "" && !invalidPattern.test(value));
   };
 
   return (
     <div className={styles.container}>
       <img src="./start.png" alt="startpic" />
       <div className={styles.search}>
-        <h1>Search Cities</h1>
+        <h1>City Search Tool</h1>
+        <p>Start typing a city name to see matching results in real-time.</p>
         <Input
           hint={hint}
           handleChange={handleChange}
           inputValue={inputValue}
+          error={error}
         />
       </div>
     </div>
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,16 +1,25 @@
-import styles from "./Input.module.css"
+import styles from "./Input.module.css";
 
-function Input({ handleChange, hint, inputValue }) {
+function Input({ handleChange, hint, inputValue, error }) {
   return (
-    <div className={`${styles["input-group"]} ${inputValue ? styles.active : ""}`}>
+    <div
+      className={`${styles["input-group"]} ${inputValue ? styles.active : ""}`}
+    >
       <label htmlFor="input">{hint}</label>
       <input
         type="text"
         id="input"
         onChange={handleChange}
         placeholder={hint}
-        
+        className={error ? styles.error : ""}
       />
+      <div className={styles.error_box}>
+        {error && (
+          <p className={styles.error_message}>
+            The entered value must contain only upper Latin letters.
+          </p>
+        )}
+      </div>
     </div>
   );
 }
